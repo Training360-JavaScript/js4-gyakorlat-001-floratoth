c0aef41233f9646ec6453032ac85a4aa25a7427a
Flow <a.flora95@gmail.com>
2021-11-18 14:07:49 +0100

    converterPromise and sorterPromise functions created

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,12 @@
+const converterPromise = (arr = []) => {
+  return new Promise((resolve, reject) => {
+    const upperCaseArr = arr.map((item) =>
+      typeof item === "string"
+        ? item.toUpperCase()
+        : reject("Error: Not all elements are string type!")
+    );
+    resolve(upperCaseArr);
+  });
+};
 
-
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,13 @@
+const sorterPromise = (arr = []) => {
+  return new Promise((resolve, reject) => {
+    const stringArr = [];
+    arr.forEach((item) =>
+      !(typeof item === "string")
+        ? reject("Error: Not all elements are string type!")
+        : stringArr.push(item)
+    );
+    resolve(stringArr.sort());
+  });
+};
 
-
-
-
-
-
-// export default sorterPromise;
+export default sorterPromise;
